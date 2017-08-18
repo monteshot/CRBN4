20f26f62d1bc26c7368348b003b18700d7990132
Сергей Кущ <faqreg@gmail.com>
2017-08-18 14:39:57 +0300

    Походу, всё

--- VrachMedcentr/ViewModel/regViewModel.cs
+++ VrachMedcentr/ViewModel/regViewModel.cs
@@ -740,7 +740,7 @@ namespace VrachMedcentr
                       }
                       catch (Exception e)
                       {
-                            MessageBox.Show(e.ToString());
+                         //   MessageBox.Show(e.ToString());
                           MessageBox.Show("Перевірте правильність введення данних");
                       }
 
